f48c4d09021c69ce5d586f66dc4d520d79a6d9cc
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2024-04-15 15:22:47 -0500

    Start having a graphical interface with Flask

--- project/projectConfig/projectConfig.py
+++ project/projectConfig/projectConfig.py
@@ -10,5 +10,9 @@ projectConfig = [
     {
         'git_url': 'https://github.com/oigonzalezp2024/python-fpdf2-table.git',
         'repo_dir' : './project/dependencies/pythonFpdf2Table'
+    },
+    {
+        'git_url': 'https://github.com/oigonzalezp2024/flaskFlow.git',
+        'repo_dir' : './project/dependencies/flaskFlow'
     }
 ]
