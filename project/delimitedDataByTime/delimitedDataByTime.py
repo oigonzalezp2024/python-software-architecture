3d617162e040033f051660c4855a9cfd952a2fa9
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2024-04-24 20:53:34 -0500

    add delimited data upgrade

--- project/delimitedDataByTime/delimitedDataByTime.py
+++ project/delimitedDataByTime/delimitedDataByTime.py
@@ -4,25 +4,40 @@ from project.dependencies.delimitedData.delimitedData import DelimitedData
 class DelimitedDataByTime(DelimitedData):
 
     def delimitedDataToOneMonth(self):
-        self.oneMonth("fechaMesIni", "./project/data/json/promedioAbasSipsaMesMadr.json", "./project/data/oneMonth/json/promedioAbasSipsaMesMadr.json")
-        self.oneMonth("fechaCaptura", "./project/data/json/promediosSipsaCiudad.json", "./project/data/oneMonth/json/promediosSipsaCiudad.json")
-        self.oneMonth("fechaMesIni", "./project/data/json/promediosSipsaMesMadr.json", "./project/data/oneMonth/json/promediosSipsaMesMadr.json")
-        self.oneMonth("enmaFecha", "./project/data/json/promediosSipsaParcial.json", "./project/data/oneMonth/json/promediosSipsaParcial.json")
-        self.oneMonth("fechaIni", "./project/data/json/promediosSipsaSemanaMadr.json", "./project/data/oneMonth/json/promediosSipsaSemanaMadr.json")
+        self.getData("./project/data/json/promedioAbasSipsaMesMadr.json")
+        self.oneMonth("fechaMesIni", pathEnd="./project/data/oneMonth/json/promedioAbasSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaCiudad.json")
+        self.oneMonth("fechaCaptura", pathEnd="./project/data/oneMonth/json/promediosSipsaCiudad.json")
+        self.getData("./project/data/json/promediosSipsaMesMadr.json")
+        self.oneMonth("fechaMesIni", pathEnd="./project/data/oneMonth/json/promediosSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaParcial.json")
+        self.oneMonth("enmaFecha", pathEnd="./project/data/oneMonth/json/promediosSipsaParcial.json")
+        self.getData("./project/data/json/promediosSipsaSemanaMadr.json")
+        self.oneMonth("fechaIni", pathEnd="./project/data/oneMonth/json/promediosSipsaSemanaMadr.json")
         return "ETL finalizada."
 
     def delimitedDataToQuarter(self):
-        self.quarter("fechaMesIni", "./project/data/json/promedioAbasSipsaMesMadr.json", "./project/data/quarter/json/promedioAbasSipsaMesMadr.json")
-        self.quarter("fechaCaptura", "./project/data/json/promediosSipsaCiudad.json", "./project/data/quarter/json/promediosSipsaCiudad.json")
-        self.quarter("fechaMesIni", "./project/data/json/promediosSipsaMesMadr.json", "./project/data/quarter/json/promediosSipsaMesMadr.json")
-        self.quarter("enmaFecha", "./project/data/json/promediosSipsaParcial.json", "./project/data/quarter/json/promediosSipsaParcial.json")
-        self.quarter("fechaIni", "./project/data/json/promediosSipsaSemanaMadr.json", "./project/data/quarter/json/promediosSipsaSemanaMadr.json")
+        self.getData("./project/data/json/promedioAbasSipsaMesMadr.json")
+        self.quarter("fechaMesIni", pathEnd="./project/data/quarter/json/promedioAbasSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaCiudad.json")
+        self.quarter("fechaCaptura", pathEnd="./project/data/quarter/json/promediosSipsaCiudad.json")
+        self.getData("./project/data/json/promediosSipsaMesMadr.json")
+        self.quarter("fechaMesIni", pathEnd="./project/data/quarter/json/promediosSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaParcial.json")
+        self.quarter("enmaFecha", pathEnd="./project/data/quarter/json/promediosSipsaParcial.json")
+        self.getData("./project/data/json/promediosSipsaSemanaMadr.json")
+        self.quarter("fechaIni", pathEnd="./project/data/quarter/json/promediosSipsaSemanaMadr.json")
         return "ETL finalizada."
 
     def delimitedDataToSemester(self):
-        self.semester("fechaMesIni", "./project/data/json/promedioAbasSipsaMesMadr.json", "./project/data/semester/json/promedioAbasSipsaMesMadr.json")
-        self.semester("fechaCaptura", "./project/data/json/promediosSipsaCiudad.json", "./project/data/semester/json/promediosSipsaCiudad.json")
-        self.semester("fechaMesIni", "./project/data/json/promediosSipsaMesMadr.json", "./project/data/semester/json/promediosSipsaMesMadr.json")
-        self.semester("enmaFecha", "./project/data/json/promediosSipsaParcial.json", "./project/data/semester/json/promediosSipsaParcial.json")
-        self.semester("fechaIni", "./project/data/json/promediosSipsaSemanaMadr.json", "./project/data/semester/json/promediosSipsaSemanaMadr.json")
+        self.getData("./project/data/json/promedioAbasSipsaMesMadr.json")
+        self.semester("fechaMesIni", pathEnd="./project/data/semester/json/promedioAbasSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaCiudad.json")
+        self.semester("fechaCaptura", pathEnd="./project/data/semester/json/promediosSipsaCiudad.json")
+        self.getData("./project/data/json/promediosSipsaMesMadr.json")
+        self.semester("fechaMesIni", pathEnd="./project/data/semester/json/promediosSipsaMesMadr.json")
+        self.getData("./project/data/json/promediosSipsaParcial.json")
+        self.semester("enmaFecha", pathEnd="./project/data/semester/json/promediosSipsaParcial.json")
+        self.getData("./project/data/json/promediosSipsaSemanaMadr.json")
+        self.semester("fechaIni", pathEnd="./project/data/semester/json/promediosSipsaSemanaMadr.json")
         return "ETL finalizada."
